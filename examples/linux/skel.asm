6cf42f6cc63378545b0c8622f8cc16d973753656
Dominic Giglio <humanshell@gmail.com>
2013-04-02 16:39:16 -0400

    simplified skel.asm

--- examples/linux/skel.asm
+++ examples/linux/skel.asm
@@ -1,25 +1,25 @@
 ; file: skel.asm
 ;
+
 %include "asm_io.inc"
+
+; initialized data is put in the data segment
 segment .data
-;
-; initialized data is put in the data segment here
-;
-segment .bss
-;
+
 ; uninitialized data is put in the bss segment
-;
+segment .bss
+
+; code is put in the text segment
 segment .text
-        global  asm_main
+global      asm_main
 asm_main:
-        enter   0,0               ; setup routine
-        pusha
+            enter   0,0               ; setup routine
+            pusha
 ;
-; code is put in the text segment. Do not modify the code before
-; or after this comment.
+; Do not modify the code before or after this comment.
 ;
-        popa
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
+            popa
+            mov     eax, 0            ; return back to C
+            leave                     
+            ret
 
