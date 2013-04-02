619d3fc43a27cf8e6e10805ca90fa24d9fed7c55
Dominic Giglio <humanshell@gmail.com>
2013-04-02 16:42:22 -0400

    tweaked comments in skel.asm

--- examples/linux/skel.asm
+++ examples/linux/skel.asm
@@ -15,9 +15,9 @@ global      asm_main
 asm_main:
             enter   0,0               ; setup routine
             pusha
-;
+;------------------------------------------------------------------------------
 ; Do not modify the code before or after this comment.
-;
+;------------------------------------------------------------------------------
             popa
             mov     eax, 0            ; return back to C
             leave                     
